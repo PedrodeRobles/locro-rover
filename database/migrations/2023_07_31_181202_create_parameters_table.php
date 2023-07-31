16e78e8f94fd1188c38175c75db2dc8cbf7c5907
Pedro de Robles <piterddr@gmail.com>
2023-07-31 15:24:38 -0300

    Creación de modelo Parameter, con controlador y migración. Config basica migración y modelo

---
+++ database/migrations/2023_07_31_181202_create_parameters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parameters', function (Blueprint $table) {
+            $table->unsignedBigInteger('year_id');
+
+            $table->integer('unit_price');
+            $table->integer('promo_unit_price');
+            $table->integer('amount_for_promo');
+            $table->timestamps();
+
+            $table->foreign('year_id')->references('id')->on('years');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parameters');
+    }
+};
