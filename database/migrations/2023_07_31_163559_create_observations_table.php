93d97a9612e11135de355629975162337e9a6471
Pedro de Robles <piterddr@gmail.com>
2023-07-31 13:38:46 -0300

    Creación de modelo Observation y migración. Config basica migración y modelo

---
+++ database/migrations/2023_07_31_163559_create_observations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('observations', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('year_id');
+
+            $table->string('observation');
+            $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('year_id')->references('id')->on('years');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('observations');
+    }
+};
