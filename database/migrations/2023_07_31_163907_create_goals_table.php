0a7d9b5dc1a4641505ccdd776eff099a88518444
Pedro de Robles <piterddr@gmail.com>
2023-07-31 14:56:47 -0300

    Creación de modelo Goal y migración. Config basica migración y modelo

---
+++ database/migrations/2023_07_31_163907_create_goals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('year_id');
+
+            $table->integer('goal');
+            $table->timestamps();
+
+            $table->foreign('year_id')->references('id')->on('years');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('goals');
+    }
+};
