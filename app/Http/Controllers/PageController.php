26f73ef5d52d93ea87533be585d4e8838ff05ce9
Pedro de Robles <pdrobles@espinlabs.com.ar>
2024-01-24 01:05:18 -0300

    Fix en 'Mi lista' respecto al array de ordenes que se enviaban

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -32,30 +32,7 @@ class PageController extends Controller
         $orders = Order::where('user_id', $user_auth_id)
         ->get()
         ->map(function($order) {
-            return [
-                'id'              => $order->id,
-                'client_id'       => $order->client->id,
-                'year_id'         => $order->year->id,
-                'user_id'         => $order->user ? $order->user->id : null,
-                'portions'        => $order->portions,
-                'take_away'       => $order->take_away,
-                'sauces'          => $order->sauces,
-                'amount'          => $order->amount,
-                'money_collected' => $order->money_collected,
-                'to_collect'      => $order->to_collect,
-                'mp'              => $order->mp,
-                'last_edition'    => $order->last_edition,
-
-                'client_name'         => $order->client->name,
-                'client_last_name'    => $order->client->last_name,
-                'client_phone_number' => $order->client->phone_number,
-                'client_direction'    => $order->client->direction,
-                'client_postal_code'  => $order->client->postal_code,
-
-                'user_name'         => $order->user ? $order->user->name : null,
-
-                'client_observations' => $order->client->observations,
-            ];
+            return OrderUtils::getOrderArray($order);
         })
         ->sortBy('id');
 
