69546331daee5bed70dcb193e9c7c8cd588ea4c3
Pedro de Robles <pdrobles@espinlabs.com.ar>
2024-04-03 02:24:18 -0300

    Feature(Application): Agregue Middleware para validar rol

--- routes/web.php
+++ routes/web.php
@@ -19,33 +19,35 @@ Route::middleware(['auth'])->group(function() {
     Route::delete('/order/{order:id}/delete', [OrderController::class, 'destroy'])->name('order.destroy');
     Route::post('/order/create', [OrderController::class, 'store'])->name('order.store');
     
-    Route::get('/clients', [ClientController::class, 'index'])->name('client.index');
-    Route::get('/client/create', [ClientController::class, 'create'])->name('client.create');
     Route::post('/client/create', [ClientController::class, 'store'])->name('client.store');
-    Route::get('/client/edit/{client:id}', [ClientController::class, 'edit'])->name('client.edit');
-    Route::put('/client/edit/{client}', [ClientController::class, 'update'])->name('client.update');
-    Route::delete('/client/delete/{client:id}', [ClientController::class, 'destroy'])->name('client.destroy');
-    Route::get('/client/import', [ClientController::class, 'importView'])->name('client.import_view');
-    Route::post('/client/import-clients', [ClientController::class, 'importExcel'])->name('client.import');
-    Route::get('/client/clientHasOrder/{client:id}', [ClientController::class, 'clientHasOrder'])->name('client.clientHasOrder');
-    Route::get('/client/deleteClients', [ClientController::class, 'deleteClients'])->name('client.deleteClients');
-    Route::put('/client/rollback/{client}', [ClientController::class, 'rollbackClient'])->name('client.rollbackClient');
     Route::put('/client/{order:id}/edit/{field}', [ClientController::class, 'updateFromOrder'])->name('client.updateFromOrder');
     Route::post('/client/{order:id}/add-order', [ClientController::class, 'addOrder'])->name('client.addOrder');
-    
-    
-    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-    Route::get('/years', [AdminController::class, 'years'])->name('admin.years');
-    Route::post('/year/store', [AdminController::class, 'createThisYear'])->name('admin.year.store');
-    Route::get('/parameters/{edit?}', [AdminController::class, 'parameters'])->name('admin.parameters');
-    Route::post('/parameters/store', [AdminController::class, 'storeParameters'])->name('admin.parameters.store');
-    Route::put('/parameters/edit/{parameter}', [AdminController::class, 'updateParameters'])->name('admin.parameters.update');
-    Route::get('/admin/export-orders', [AdminController::class, 'exportOrders'])->name('admin.export-orders');
-    Route::get('/admin/roles', [AdminController::class, 'roles'])->name('admin.roles');
-    Route::put('/admin/assign_roles/{user_id}/{role_name}', [AdminController::class, 'assignRoles'])->name('admin.assign.roles');
-    Route::put('/admin/users/{user_id}/softDelete', [AdminController::class, 'softDelete'])->name('admin.users.soft-delete');
-    Route::get('/admin/users/softDeleted', [AdminController::class, 'softDeletedUsers'])->name('admin.users.soft-deleted');
-    Route::put('/admin/users/{user_id}/rollbackUser', [AdminController::class, 'rollbackUser'])->name('admin.users.rollback-user');
+
+    Route::middleware(['role:Admin,Logistica'])->group(function() {
+        Route::get('/clients', [ClientController::class, 'index'])->name('client.index');
+        Route::get('/client/create', [ClientController::class, 'create'])->name('client.create');
+        Route::get('/client/edit/{client:id}', [ClientController::class, 'edit'])->name('client.edit');
+        Route::put('/client/edit/{client}', [ClientController::class, 'update'])->name('client.update');
+        Route::delete('/client/delete/{client:id}', [ClientController::class, 'destroy'])->name('client.destroy');
+        Route::get('/client/import', [ClientController::class, 'importView'])->name('client.import_view');
+        Route::post('/client/import-clients', [ClientController::class, 'importExcel'])->name('client.import');
+        Route::get('/client/clientHasOrder/{client:id}', [ClientController::class, 'clientHasOrder'])->name('client.clientHasOrder');
+        Route::get('/client/deleteClients', [ClientController::class, 'deleteClients'])->name('client.deleteClients');
+        Route::put('/client/rollback/{client}', [ClientController::class, 'rollbackClient'])->name('client.rollbackClient');
+
+        Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+        Route::get('/years', [AdminController::class, 'years'])->name('admin.years');
+        Route::post('/year/store', [AdminController::class, 'createThisYear'])->name('admin.year.store');
+        Route::get('/parameters/{edit?}', [AdminController::class, 'parameters'])->name('admin.parameters');
+        Route::post('/parameters/store', [AdminController::class, 'storeParameters'])->name('admin.parameters.store');
+        Route::put('/parameters/edit/{parameter}', [AdminController::class, 'updateParameters'])->name('admin.parameters.update');
+        Route::get('/admin/export-orders', [AdminController::class, 'exportOrders'])->name('admin.export-orders');
+        Route::get('/admin/roles', [AdminController::class, 'roles'])->name('admin.roles');
+        Route::put('/admin/assign_roles/{user_id}/{role_name}', [AdminController::class, 'assignRoles'])->name('admin.assign.roles');
+        Route::put('/admin/users/{user_id}/softDelete', [AdminController::class, 'softDelete'])->name('admin.users.soft-delete');
+        Route::get('/admin/users/softDeleted', [AdminController::class, 'softDeletedUsers'])->name('admin.users.soft-deleted');
+        Route::put('/admin/users/{user_id}/rollbackUser', [AdminController::class, 'rollbackUser'])->name('admin.users.rollback-user');
+    });
 });
 
 
