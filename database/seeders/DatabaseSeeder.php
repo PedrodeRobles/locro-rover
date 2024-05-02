41da883fcf9680ca5f102638ec4b14c55285152a
Pedro de Robles <pdrobles@espinlabs.com.ar>
2024-05-02 07:27:44 -0300

    Feature(Application): Agregue datos default en el seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Year;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,16 +18,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Year::factory(1)->create();
-        \App\Models\Goal::factory(1)->create();
-        \App\Models\Parameter::factory(1)->create();
-        \App\Models\Client::factory(10)->create();
-        \App\Models\Order::factory(60)->create();
-        \App\Models\Observation::factory(20)->create();
+        // \App\Models\Year::factory(1)->create();
+        // \App\Models\Goal::factory(1)->create();
+        // \App\Models\Parameter::factory(1)->create();
+        // \App\Models\Client::factory(10)->create();
+        // \App\Models\Order::factory(60)->create();
+        // \App\Models\Observation::factory(20)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'piterddr@gmail.com',
+            'active' => 1,
+            'email_verified_at' => now(),
+            'password' => bcrypt(12345678), // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
+
+        Year::create([
+            'year'   => Carbon::now()->year,
+            'active' => true
+        ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => '1',
+            'model_type' => 'App\Models\User',
+            'model_id' => $user->id,
+        ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => '8',
+            'model_type' => 'App\Models\User',
+            'model_id' => $user->id,
+        ]);
     }
 }
