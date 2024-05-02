5e498945a7c053e7336a7442934d4af3e0889518
Pedro de Robles <pdrobles@espinlabs.com.ar>
2024-05-02 06:06:18 -0300

    Feature(Proyecto general): Agregue icono + Titulo de pestaña + Rollback de cambios en composer y npm, respecto a websockets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="shortcut icon" href="{{ asset('images/rover-logo.png') }}" type="image/x-icon">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/rover-logo.png') }}">
 
         <!-- Scripts -->
         @routes
